185328fdc92a11461f5e87be7fd2fc8030a44bae
sanjai0py <sanjailucifer666@gmail.com>
2023-09-06 16:05:10 +0530

    changed js

--- client/script.js
+++ client/script.js
@@ -1,14 +1,6 @@
 "use strict";
-import  "./js/showdown.min.js";
-
 
 const age = document.getElementById("age");
-
-var converter = new showdown.Converter(),
-  text = "# hello, markdown!",
-  html = converter.makeHtml(text);
-
-console.log(html);
 
 function calcAge() {
   const currentYear = new Date().getFullYear();
